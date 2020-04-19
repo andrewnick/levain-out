ce42a64f4f4e71f0c5c2b6ca874f8fcf7fc1e7db
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2020-04-19 04:59:01 +0100

    feat: add apollo to server and client. add tailwind.css

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,36 +1,39 @@
 import { ApolloServer, gql } from 'apollo-server-micro'
-// import five from "johnny-five";
-// import { RaspiIO } from "raspi-io";
+import five from "johnny-five";
+import { RaspiIO } from "raspi-io";
+import Cors from "micro-cors";
 
+var board = new five.Board({
+  io: new RaspiIO()
+});
 
+board.on("ready", function () {
+  var led = new five.Led("P1-13");
+  led.blink();
+});
 
-// board.on("ready", function () {
-//   var led = new five.Led("P1-13");
-//   led.blink();
-// });
-
-const typeDefs = gql`
-  type Query {
-    users: [User!]!
-  }
-  type User {
-    name: String
-  }
-`
 // const typeDefs = gql`
 //   type Query {
 //     users: [User!]!
 //   }
-//   // type Mutation {
-//   //   setLed(status: String!): Led
-//   // }
 //   type User {
 //     name: String
 //   }
-//   // type Led {
-//   //   status: String
-//   // }
 // `
+const typeDefs = gql`
+  type Query {
+    users: [User!]!
+  }
+  type Mutation {
+    setLed: Led
+  }
+  type User {
+    name: String
+  }
+  type Led {
+    status: String
+  }
+`
 
 const resolvers = {
   Query: {
@@ -41,30 +44,37 @@ const resolvers = {
 
       return [{ name: 'Nextjs' }]
     },
+  },
+  Mutation: {
+    setLed(root, args) {
+      // const board = new five.Board({
+      //   io: new RaspiIO()
+      // });
+      // var led = new five.Led("P1-13");
+
+      // if (args.status == 'on') {
+      //   board.on("ready", function () {
+      //     led.on();
+      //   });
+      // } else {
+      //   board.on("ready", function () {
+      //     led.off();
+      //   });
+      // }
+      console.log('test');
+
+      return { status: 'yes' };
+    },
   }
-  // Mutation: {
-  //   setLed: (root, args) => {
-  //     // const board = new five.Board({
-  //     //   io: new RaspiIO()
-  //     // });
-  //     // var led = new five.Led("P1-13");
-
-  //     // if (args.status == 'on') {
-  //     //   board.on("ready", function () {
-  //     //     led.on();
-  //     //   });
-  //     // } else {
-  //     //   board.on("ready", function () {
-  //     //     led.off();
-  //     //   });
-  //     // }
-
-  //     return { status: args.status };
-  //   },
-  // }
 }
 
+const cors = Cors({
+  allowMethods: ["GET", "POST", "OPTIONS"]
+});
+
 const apolloServer = new ApolloServer({ typeDefs, resolvers })
+
+const handler = apolloServer.createHandler({ path: '/api/graphql' });
 
 export const config = {
   api: {
@@ -72,4 +82,4 @@ export const config = {
   },
 }
 
-export default apolloServer.createHandler({ path: 'http://localhost:3000/api/graphql' })
+export default cors(handler);
