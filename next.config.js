ba7dacf0a66325be1ad2f7b87a9297104e05e246
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2021-08-09 07:28:33 +0100

    feat: next config

---
+++ next.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+    // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    //     // console.log(__dirname);
+    //     // // console.log(__dirname, "/node_modules/typeorm/typeorm-model-shim");
+    //     // config.resolve.alias = {
+    //     //     ...config.resolve.alias,
+    //     //     ...{ "typeorm": path.resolve(__dirname, "../node_modules/typeorm/typeorm-model-shim") }
+    //     // }
+    //     // console.log(config.resolve.alias);
+    //     // config.resolve = {
+    //     //     alias: {
+    //     //         typeorm: path.resolve(__dirname, "node_modules/typeorm/typeorm-model-shim")
+    //     //     }
+    //     // }
+    //     return config
+    // }
+}
