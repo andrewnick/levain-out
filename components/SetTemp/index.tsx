d861a3e4e76d5ebd854d3336013aeeebee0335d1
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2021-08-22 02:54:38 +0100

    fix: typeerror

--- components/SetTemp/index.tsx
+++ components/SetTemp/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { mutation } from "../../graphql/gqlClient";
-import { Setting } from "@/types";
+import { Setting } from "@/types/global";
 import {
   Formik,
   Form,
