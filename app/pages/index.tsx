a10117b09e7f3a9c0b01f48c4c5c1f188c870c83
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2022-03-18 12:08:41 +1300

    chore: tidy up

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -8,37 +8,10 @@ import Info from "../components/Info";
 import Card from "../components/Card";
 import SetTemp from "../components/SetTemp";
 import RecordingControl from "../components/RecordingControl";
-import { query } from "../graphql/gqlClient";;
-import {  Log, Setting, Session } from "../../types/global";
+import { query } from "../graphql/gqlClient";
+import { Log, Setting, Session } from "../../types/global";
 import useSWR, { useSWRInfinite } from "swr";
-// import { SWRInfiniteKeyLoader } from 'swr/infinite'
-
-const Graph = dynamic(() => import("../components/Graph"), {
-  ssr: false,
-});
-
-interface IndexServerSideProps {
-  log: Log;
-  logs: Array<Log>;
-  setting: Setting;
-}
-
-export const NAME_QUERY = `
-  query {
-    setting {
-      set_point
-      set_point_tolerance
-    }
-  }
-`;
-
-const SET_LED = `
-  mutation setLed  {
-    setLed {
-      status
-    }
-  }
-`;
+import Graph from "../components/Graph";
 
 const GET_LOGS = `
   query {
@@ -60,13 +33,23 @@ const GET_SESSION = `
   }
 `;
 
-function createPaginatedLogsQuery ({limit = 10, afterCursor = null, beforeCursor = null}: {limit?: number, afterCursor?: string | null, beforeCursor?: string | null }) {
-  console.log({limit, afterCursor, beforeCursor});
+function createPaginatedLogsQuery({
+  sessionId,
+  limit = 10,
+  afterCursor = null,
+  beforeCursor = null,
+}: {
+  sessionId: number;
+  limit?: number;
+  afterCursor?: string | null;
+  beforeCursor?: string | null;
+}) {
+  // console.log({limit, afterCursor, beforeCursor});
 
-  const afC = afterCursor ? `"${afterCursor}"` : null
+  const afC = afterCursor ? `"${afterCursor}"` : null;
 
   return `query {
-            paginatedLogs(limit: ${limit}, cursor: {
+            paginatedLogs(sessionId: ${69}, limit: ${limit}, cursor: {
               beforeCursor: ${beforeCursor},
               afterCursor: ${afC},
             }) {
@@ -81,7 +64,7 @@ function createPaginatedLogsQuery ({limit = 10, afterCursor = null, beforeCursor
                 switch
               }
             }
-          }`
+          }`;
 }
 
 // interface Home {
@@ -99,56 +82,85 @@ function createPaginatedLogsQuery ({limit = 10, afterCursor = null, beforeCursor
 //   return useState(isRecording);
 // }
 
+const getKey = (pageIndex, previousPageData, sessionId, limit) => {
+  // console.log('getKey');
+  // console.log({ pageIndex });
+  // console.log({ previousPageData });
+  // console.log({ sessionId });
+  if (pageIndex === 0) return createPaginatedLogsQuery({ sessionId, limit });
 
-const getKey = (pageIndex, previousPageData) => {
-  console.log('getKey');
-  
-  console.log({ pageIndex });
-  console.log({ previousPageData });
-  // if (previousPageData?.paginatedLogs?.cursor?.afterCursor == null) return null
+  if (
+    previousPageData &&
+    previousPageData?.paginatedLogs?.cursor?.afterCursor == null
+  )
+    return null;
 
-  const afterCursor  = previousPageData?.paginatedLogs?.cursor?.afterCursor ?? null
-  // if (previousPageData.paginatedLogs?.cursor?.afterCursor == null) return createPaginatedLogsQuery({})
-  console.log({ afterCursor });
+  const afterCursor =
+    previousPageData?.paginatedLogs?.cursor?.afterCursor ?? null;
 
-  const query = createPaginatedLogsQuery({ afterCursor })
-  // const query = createPaginatedLogsQuery({})
-  // console.log({query});
-
-  return query
-}
+  const query = createPaginatedLogsQuery({ sessionId, afterCursor, limit });
+  return query;
+};
 
 const Home = () => {
-  // const { data, error } = useSWR<{ session: Session }, boolean>(
-  //   GET_SESSION,
-  //   query,
-  //   {
-  //     refreshInterval: 9000,
-  //   }
-  // );
-console.log(createPaginatedLogsQuery({}));
+  const [indexSize, setIndexSize] = useState(8000);
+  const [logPageSize, setLogPageSize] = useState(3);
+  const { data: sessionData, error: sessionError } = useSWR<
+    { session: Session },
+    boolean
+  >(GET_SESSION, query);
 
-  const { data, error, isValidating, mutate, size, setSize } = useSWRInfinite(getKey, query, { persistSize: false })
+  const session = sessionData?.session;
 
-  // const [recording, setRecording] = useState(false);
-  // useEffect(() => {
-  //   setRecording(data?.session?.status === 'started');
-  // }, [data])
-  
+  const {
+    data: logData,
+    error,
+    isValidating,
+    mutate,
+    size,
+    setSize,
+  } = useSWRInfinite(
+    (pageIndex, previousPageData) =>
+      getKey(pageIndex, previousPageData, session.id, indexSize),
+    query,
+    {
+      initialSize: logPageSize,
+    }
+  );
+
+  console.log({ size });
+
+  const [recording, setRecording] = useState(false);
+  useEffect(() => {
+    setRecording(sessionData?.session?.status === "started");
+  }, [sessionData]);
+
   // if (error) return <div>failed to load</div>
   // if (!data) return <div>loading...</div>
- 
+
   // const { session } = data
   // const { logs } = session
-  console.log({ data, error, isValidating, mutate, size, setSize });
+  console.log({ logData, error, isValidating, mutate, size, setSize });
   useEffect(() => {
-    const timer = setTimeout(() => setSize(10), 1000);
-    return () => clearTimeout(timer);
-  }, []);
+    let timer;
+    if (logData) {
+      const lastLogSet = logData[logData.length - 1];
+      const moreLogs = lastLogSet.paginatedLogs.cursor.afterCursor !== null;
+      timer = setTimeout(() => {
+        if (moreLogs) {
+          setSize(logPageSize);
+          setLogPageSize(logPageSize + 1);
+          setIndexSize(10);
+        }
+      }, 200);
+    }
 
-  const logs = data && data.flatMap(value => value.paginatedLogs.logs);
-  console.log({logs});
-  
+    return () => clearTimeout(timer);
+  }, [logPageSize, logData]);
+
+  const logs = logData && logData.flatMap((value) => value.paginatedLogs.logs);
+  // console.log({logData[logData.length - 1]});
+  console.log({ logs });
 
   return (
     <div>
@@ -161,13 +173,16 @@ console.log(createPaginatedLogsQuery({}));
         <div className="flex flex-wrap">
           <div className="flex-initial w-100 min-w-full mb-8">
             <Card>
-              {Boolean(logs?.length) &&  (
+              {Boolean(logs?.length) && (
                 <>
                   <Info logs={logs} />
                   {Boolean(logs?.length) && <Graph logs={logs} />}
                 </>
               )}
-              {/* <RecordingControl recording={recording} setRecording={setRecording} /> */}
+              <RecordingControl
+                recording={recording}
+                setRecording={setRecording}
+              />
             </Card>
           </div>
           {/* <div className="flex-initial w-full sm:w-1/2 mb-8 sm:pr-4">
